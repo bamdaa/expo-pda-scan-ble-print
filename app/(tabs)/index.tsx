123ee29ed540ab900faa102fc9c9fb390d224adf
Galbadrakh Ulzii <bamdaa@yahoo.com>
2025-03-13 19:58:34 +0800

    PDA Scanner done.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,89 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { useEffect, useState } from "react"
+import {
+	Text,
+	View,
+	StyleSheet,
+	type EmitterSubscription,
+	NativeEventEmitter,
+	Platform,
+} from "react-native"
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import Urovo, {
+	closeScanner,
+	openScanner,
+	UROVO_EVENTS,
+	type ScanResult,
+} from "react-native-urovo"
+
+import { useKeyEvent } from "@/hooks/useKeyEvent"
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+	const { keyEvent } = useKeyEvent()
+
+	const [scanResult, setScanResult] = useState<ScanResult>()
+	const [isScannerOpened, setIsScannerOpened] = useState<boolean>(false)
+
+	useEffect(() => {
+		let isMounted = true
+
+		const open = async () => {
+			const isOpened = await openScanner()
+
+			if (isMounted) {
+				setIsScannerOpened(!!isOpened)
+			}
+		}
+
+		open()
+
+		return () => {
+			isMounted = false
+			closeScanner()
+		}
+	}, [])
+
+	useEffect(() => {
+		let eventListener: EmitterSubscription | undefined
+
+		if (isScannerOpened && Urovo) {
+			const eventEmitter =
+				Platform.OS === "android"
+					? new NativeEventEmitter()
+					: new NativeEventEmitter(Urovo)
+
+			if (!eventListener)
+				eventListener = eventEmitter.addListener(
+					UROVO_EVENTS.ON_SCAN,
+					(scan: ScanResult) => {
+						console.log("Scan result:", scan)
+
+						setScanResult(scan)
+					}
+				)
+		}
+
+		return () => {
+			eventListener?.remove()
+		}
+	}, [isScannerOpened])
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Result: {scanResult?.value}</Text>
+			<Text style={styles.text}>Type: {scanResult?.type}</Text>
+			<Text style={styles.text}>Key: {keyEvent?.key}</Text>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	text: {
+		fontSize: 20,
+		textAlign: "center",
+	},
+})
---
+++ hooks/useKeyEvent.ts
@@ -0,0 +1,42 @@
+import { useEvent } from "expo"
+import { useEffect, useMemo } from "react"
+
+import ExpoKeyEventModule from "expo-key-event/src/ExpoKeyEventModule"
+
+/**
+ *
+ * @param listenOnMount Pass 'false' to prevent automatic key event listening
+ * - Use startListening/stopListening to control the listener manually
+ * @returns
+ *
+ */
+export function useKeyEvent(listenOnMount = true) {
+	const event = useEvent(ExpoKeyEventModule, "onKeyPress")
+
+	useEffect(() => {
+		if (listenOnMount) ExpoKeyEventModule.startListening()
+
+		return () => {
+			ExpoKeyEventModule.stopListening()
+		}
+	}, [listenOnMount])
+
+	const keyEvent = useMemo(() => {
+		if (!event) return null
+		return {
+			key: event.key,
+		}
+	}, [event])
+
+	return {
+		/**
+		 * Start listening for key events
+		 */
+		startListening: () => ExpoKeyEventModule.startListening(),
+		/**
+		 * Stop listening for key events
+		 */
+		stopListening: () => ExpoKeyEventModule.stopListening(),
+		keyEvent,
+	}
+}
